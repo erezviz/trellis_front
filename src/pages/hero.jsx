876856a1cc6b85f4517858bfc8e7ed625efe995b
shlomo lifschitz <shinlamed@gmail.com>
2022-05-25 12:54:58 +0300

    changed Hero

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -5,7 +5,8 @@ export const Hero = () => {
     return (
         <section className="hero">
             <div className="hero-container">
-                <h1>Hello Hero</h1>
+                <h1>Welcome to Trelis</h1>
+                <button className="hero-btn"></button>
             </div>
         </section>
     )
