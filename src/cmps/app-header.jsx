c8eda0ee5904394bc208468be6a141d02e7010f3
erezviz <erezviz@gmail.com>
2022-05-25 14:28:22 +0300

    basic header assembly done

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -2,15 +2,24 @@ import { NavLink, Link } from "react-router-dom"
 
 
 export const AppHeader = (props) => {
-
+    
 
     return (
-        <section className="app-header">
+        <section className="app-header flex">
+         
+            <div className="logo-container">
+                <h3>Trellis</h3>
+            </div>
             <nav>
-                <Link to='/'>Trellis</Link>| 
-                <Link to='/home'>boards</Link>|
+            
+                <Link  to='/home'><p className="header-link">Boards</p></Link>
             </nav>
-            <h1>Hello from Header</h1>
+            <div  className="user-link">
+                <span>EW</span>
+            
+            </div>
+           
         </section>
     )
 }
+
