435c773cb3b086fdf732b59ff08682612868beae
shlomo lifschitz <shinlamed@gmail.com>
2022-05-25 14:11:34 +0300

    hero changes

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 
 export const Hero = () => {
@@ -5,8 +6,12 @@ export const Hero = () => {
     return (
         <section className="hero">
             <div className="hero-container">
-                <h1>Welcome to Trelis</h1>
-                <button className="hero-btn"></button>
+                <h1>Trello helps teams move work forward.</h1>
+                <p>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello.</p>
+                <Link to='/home'><div className="enter-link">Enter Demo</div></Link>
+            </div>
+            <div className="hero-img-container">
+                <img className="hero-img" src={`https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/spirit/hero/6a3ccd8e5c9a0e8ebea4235d12da6b24/hero.png`} alt="" />
             </div>
         </section>
     )
