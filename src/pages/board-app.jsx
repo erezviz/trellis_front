8f6522d588e6a0d3895b4bdee83facbf1120679d
shlomo lifschitz <shinlamed@gmail.com>
2022-05-26 08:58:33 +0300

    hey

--- src/pages/board-app.jsx
+++ src/pages/board-app.jsx
@@ -1,75 +1,71 @@
 import React from "react";
 import { GroupPreview } from "../cmps/group-preview";
 import { TaskDetails } from "../cmps/task-details";
-import {GroupList} from "../cmps/group-list"
+import { GroupList } from "../cmps/group-list"
 import { boardService } from "../services/board.service";
 import { utilService } from "../services/util.service";
 
-export class BoardApp extends React.Component{
+export class BoardApp extends React.Component {
 
-    state={
+    state = {
         groups: []
     }
 
-    componentDidMount=()=>{
+    componentDidMount = () => {
         this.loadGroups()
     }
-    
-    loadGroups=async(board)=>{
+
+    loadGroups = async (board) => {
         const boardId = this.getBoardId()
-        if(!board){
-        try{
-            const board = await boardService.getById(boardId)
-            this.setState({groups: [...board.groups]})
-        }catch(err){
-            throw err
-        }  
-    }else this.setState({groups: [...board.groups]})
-}
-    getBoardId=()=>{
-         const {boardId} = (this.props.match.params)
+        if (!board) {
+            try {
+                const board = await boardService.getById(boardId)
+                this.setState({ groups: [...board.groups] })
+            } catch (err) {
+                throw err
+            }
+        } else this.setState({ groups: [...board.groups] })
+    }
+    getBoardId = () => {
+        const { boardId } = (this.props.match.params)
         return boardId
     }
 
-    onDeleteGroup=async(groupId)=>{
+    onDeleteGroup = async (groupId) => {
         const boardId = this.getBoardId()
-        try{
+        try {
             const board = await boardService.deleteGroup(boardId, groupId)
             this.loadGroups(board)
-        }catch(err){
+        } catch (err) {
             throw err
         }
     }
 
-    onAddGroup=async ()=>{
+    onAddGroup = async () => {
         const boardId = this.getBoardId()
-        const groupName  = prompt('Group name?')
-        const newGroup = {id: "G-"+utilService.makeId(), title: groupName}
-        try{
-           const board = await boardService.addGroup(boardId, newGroup)
-           this.loadGroups(board)
-        }catch(err){
+        const groupName = prompt('Group name?')
+        const newGroup = { id: "G-" + utilService.makeId(), title: groupName }
+        try {
+            const board = await boardService.addGroup(boardId, newGroup)
+            this.loadGroups(board)
+        } catch (err) {
             throw err
-        }finally {
+        } finally {
         }
     }
 
-    render(){
-        const {groups} = this.state
+    render() {
+        const { groups } = this.state
         return (
             <section className="board-app">
                 <section className="task-details-overlay">
-                    <TaskDetails/>
+                    <TaskDetails />
                 </section>
                 <button onClick={this.onAddGroup}>Add Group</button>
                 {(!groups || !groups.length) && <h3>Loading...</h3>}
-                {/* {groups && groups.length &&  */}
-                    
-                       <div className="list-container">
-                  <GroupList groups={groups} />
-                   </div>
-                   
-                {/* } */}
+                <div className="list-container">
+                    <GroupList groups={groups} />
+                </div>
             </section>
         )
     }
