91aa252fbe59fb235a5b28436fd87511d106f6f6
shlomo lifschitz <shinlamed@gmail.com>
2022-05-27 18:10:12 +0300

    input for add group

--- src/cmps/group/group-footer.jsx
+++ src/cmps/group/group-footer.jsx
@@ -1,62 +1,53 @@
 import { TextField } from "@mui/material"
-import React from "react"
+import React, { useState } from "react"
 import { boardService } from "../../services/board.service"
-import { connect } from "react-redux"
-
-class _GroupFooter extends React.Component {
-    state = {
-        task: boardService.getEmptyTask(),
-        isShown: true
-    }
+import { useDispatch, useSelector } from "react-redux"
+import { updateBoard } from "../../store/board.action"
 
 
-    onHandleChange = ({ target }) => {
-        const field = target.name
-        this.setState((prevState) => ({
-            task: { ...prevState.task, [field]: target.value },
-        }))
-    }
-
-    onSaveTask = (ev) => {
-        ev.preventDefault()
-        const taskToSave = { ...this.state.task }
-        const boardId = this.props.boardId
-        const groupId = this.props.groupId
-        boardService.saveTask(boardId, groupId, taskToSave)
-        this.setState({ isShown: true })
-    }
-
+export const GroupFooter = (props) => {
+    const dispatch = useDispatch()
+    const [isShown, setIsShown] = useState(true)
+    var task = { title: '' }
+    const { currBoard } = useSelector(state => state.boardModule)
    
 
-    onToggle = () => {
-        this.setState({ isShown: false })
+    const onHandleChange = ({ target }) => {
+        task = { title: target.value }
+    }
+
+    const onSaveTask = async (ev) => {
+        ev.preventDefault()
+        const boardId = props.boardId
+        const groupId = props.groupId
+        const updatedBoard = await boardService.saveTask(boardId, groupId, task)
+        dispatch(updateBoard(updatedBoard))
+        setIsShown(true)
     }
 
 
-    render() {
-  
-        const { isShown } = this.state
-        return (
-            <section className="group-footer">
-                {isShown && <div onClick={this.onToggle} className="add-task">
-                    <span className="plus-task">+</span>
-                    <span>Add a card</span>
-                </div>}
-                {!isShown && <form onSubmit={(ev)=>this.onSaveTask(ev)}>
-                    <TextField name="title" id="outlined-basic" onChange={this.onHandleChange}
-                        size="small" placeholder="Enter a title for this card..." variant="outlined" />
-                    <button className='btn-save'>Save card</button>
-                </form>}
 
-            </section>
-        )
+    const onToggle = () => {
+        setIsShown(false)
     }
+
+
+
+
+    return (
+        <section className="group-footer">
+            {isShown && <div onClick={onToggle} className="add-task">
+                <span className="plus-task">+</span>
+                <span>Add a card</span>
+            </div>}
+            {!isShown && <form onSubmit={(ev) => onSaveTask(ev)}>
+                <TextField name="title" id="outlined-basic" onChange={onHandleChange}
+                    size="small" placeholder="Enter a title for this card..." variant="outlined" />
+                <button className='btn-save'>Save card</button>
+            </form>}
+
+        </section>
+    )
+
 }
 
-function mapStateToProps(state) {
-    return {
-        board: state.boardModule
-    }
-}
-
-export const GroupFooter = connect(mapStateToProps)(_GroupFooter)
--- src/pages/board-app.jsx
+++ src/pages/board-app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from "react-redux"
 import TextField from '@mui/material/TextField';
 
@@ -10,7 +10,7 @@ import { GroupList } from "../cmps/group/group-list"
 import { boardService } from "../services/board.service";
 import { utilService } from "../services/util.service";
 import { loadBoard } from '../store/board.action'
-import {Screen} from '../cmps/dynamic-cmps/screen'
+import { Screen } from '../cmps/dynamic-cmps/screen'
 import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import { formatMuiErrorMessage } from "@mui/utils";
@@ -21,7 +21,8 @@ class _BoardApp extends React.Component {
     state = {
         groups: [],
         isModalOpen: false,
-        isShown: true
+        isShown: false,
+        newGroup: { title: '' }
     }
 
     componentDidMount = () => {
@@ -54,24 +55,27 @@ class _BoardApp extends React.Component {
         }
     }
 
-    onAddGroup = async () => {
+    onAddGroup = async (ev) => {
+        ev.preventDefault()
+        const { newGroup } = this.state
+        console.log(newGroup)
         const boardId = this.getBoardId()
-        const groupName = prompt('Group name?')
-        const newGroup = { id: "G-" + utilService.makeId(), title: groupName }
+         newGroup.id =utilService.makeId()
         try {
             const board = await boardService.addGroup(boardId, newGroup)
             this.loadGroups(board)
         } catch (err) {
             throw err
         } finally {
+            this.setState({ isShown: false })
         }
     }
 
     onHandleChange = ({ target }) => {
-        console.log(target.value);
         const field = target.name
+        console.log(field);
         this.setState((prevState) => ({
-            task: { ...prevState.groups, [field]: target.value },
+            task: { ...prevState.newGroup, [field]: target.value },
         }))
     }
 
@@ -79,54 +83,60 @@ class _BoardApp extends React.Component {
 
         this.setState(prevState => ({
             isModalOpen: !prevState.isModalOpen
-          }));
-      
+        }));
+
     }
 
     onCloseDetails = (ev) => {
         // ev.stopPropagation()
-      
+
         // this.props.history.goBack()
         this.onToggleDetails()
     }
 
+    onToggleGroup = () => {
+        this.setState({ isShown: !this.state.isShown })
+    }
+
     render() {
-    
+
         const { boardId } = this.props.match.params
 
- 
+
         const { groups, isModalOpen, isShown } = this.state
         // console.log('isModalOpen? ',isModalOpen);
         return (
             <section className="board-app">
-              
+
                 {/* <section onClick={this.onToggleDetails} className={`task-details-overlay ${isModalOpen ? 'overlay-up' : ''} `}>
                 </section> */}
                 {(!groups || !groups.length) && <h3>Loading...</h3>}
                 <div className="list-container">
                     <GroupList boardId={boardId} onToggleDetails={this.onToggleDetails} onDeleteGroup={this.onDeleteGroup} groups={groups} />
-               
+
                 </div>
 
                 <>
-                {/* <Route path='/board/:boardId/:groupId/:taskId' component={TaskDetails}>
+                    {/* <Route path='/board/:boardId/:groupId/:taskId' component={TaskDetails}>
                 <Screen isOpen={isModalOpen}   >
                     <TaskDetails isOpen={isModalOpen} onToggleDetails={this.onToggleDetails} />
 
                 </Screen>
                 </Route> */}
-                <Route path='/board/:boardId/:groupId/:taskId' >
-                <Screen isOpen={isModalOpen}   >
-                    <TaskDetails isOpen={isModalOpen} onToggleDetails={this.onToggleDetails} />
+                    <Route path='/board/:boardId/:groupId/:taskId' >
+                        <Screen isOpen={isModalOpen}   >
+                            <TaskDetails isOpen={isModalOpen} onToggleDetails={this.onToggleDetails} />
 
-                </Screen>
-                </Route>
+                        </Screen>
+                    </Route>
                 </>
                 <div className="add-group">
-              {!isShown &&  <button className="add-group-btn" onClick={this.onAddGroup}><span className="plus">+</span> Add another list</button>}
-              {isShown && <form >
-              <TextField onChange={this.onHandleChange} name="title"id="outlined-textarea" placeholder="Enter list title..." multiline />
-              </form> }
+                    {!isShown && <button className="add-group-btn" onClick={this.onToggleGroup}><span className="plus">+</span> Add another list</button>}
+                    {isShown && <form onSubmit={(ev)=>this.onAddGroup(ev)}>
+                        <TextField onChange={this.onHandleChange} name="title" id="outlined-textarea" placeholder="Enter list title..." multiline />
+                        <button >save list</button>
+                        <button onClick={this.onToggleGroup}>x</button>
+                    </form>}
                 </div>
             </section>
         )
