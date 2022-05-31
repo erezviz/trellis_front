f694c18f0747d50acac1a6c9007999f994d79e97
shlomo lifschitz <shinlamed@gmail.com>
2022-05-31 15:07:33 +0300

    user changes

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -3,33 +3,33 @@ import { NavLink, Link } from "react-router-dom"
 
 
 
- const _AppHeader = (props) => {
-
+const _AppHeader = (props) => {
+    const { user } = useSelector((state) => state.userModule)
 
     const onGoBack = () => {
         props.history.push('/')
-        
+
     }
 
-    
+
     return (
         <section className="app-header flex">
-         
+
             <div onClick={onGoBack} className="logo-container">
 
-            <img src={require('../assets/img/trellis.png')} alt=""  /> <h3>Trellis</h3>
+                <img src={require('../assets/img/trellis.png')} alt="" /> <h3>Trellis</h3>
             </div>
             <nav>
-            
+
                 {/* <Link  to='/home'><p className="header-link">Boards</p></Link> */}
             </nav>
-           {!user && <Link to={'/login'}>
-            <div  className="user-link">
-                <span className="login">Log In</span>
-                <span className="signup">Sign Up</span>
-            </div>
+            {!user && <Link to={'/login'}>
+                <div className="user-link">
+                    <span className="login">Log In</span>
+                    <span className="signup">Sign Up</span>
+                </div>
             </Link>}
-           
+
         </section>
     )
 }
