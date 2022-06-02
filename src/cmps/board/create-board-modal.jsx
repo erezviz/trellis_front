5383d03bfd4eee3f561861a1bfdba2fc0849d29b
erezviz <erezviz@gmail.com>
2022-06-02 08:43:50 +0300

    added ui createboardmodal

--- src/cmps/board/create-board-modal.jsx
+++ src/cmps/board/create-board-modal.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
-import {useDispatch} from "react-redux"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
 import { useHistory } from 'react-router-dom'
 import { addBoard } from "../../store/board.action"
 export const CreateBoardModal = ({ isShown }) => {
     const dispatch = useDispatch()
-   const history = useHistory()
+    const [isTyping, setIsTyping] = useState(false)
+    const history = useHistory()
     const [board, setBoard] = useState({
         title: '',
         createdBy: '',
@@ -23,22 +24,30 @@ export const CreateBoardModal = ({ isShown }) => {
         board.createdAt = Date.now()
         const addedBoard = await dispatch(addBoard(board))
         history.push(`/board/${addedBoard._id}`)
-        console.log('board in onadd', board); 
+
+    }
+
+    const onTyping = ev => {
+        const { length } = ev.target.value
+        if (length === 0) setIsTyping(false)
+        else if (length > 0) setIsTyping(true)
+
 
     }
     return (
 
         <section className="create-board-modal" style={{ display: `${isShown ? 'block' : 'none'}` }}>
-            <form onSubmit={onAddBoard}  className="create-board-title flex">
+            <form onSubmit={onAddBoard} className="create-board-title flex">
                 <label htmlFor="title">Board title<span>*</span></label>
                 <input
+                    onInput={onTyping}
                     onChange={onHandleChange}
                     type="text"
                     name="title"
                     id="title"
                     required
                 />
-                <button className="btn btn-blue">Create</button>
+                <button className={`btn btn${isTyping ? '-blue' : '-light'}`}>Create</button>
             </form>
         </section>
 
