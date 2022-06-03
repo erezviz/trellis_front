0eb6e545951cf6f1a964f6a98ce2a16780e66688
NoyaItzhaki <noinoya@gmail.com>
2022-06-03 11:10:27 +0300

    home design

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -12,6 +12,8 @@ import {
 import { BoardPreview } from '../cmps/board/board-preview'
 import { PopOver } from "../cmps/dynamic-cmps/pop-over";
 import { CreateBoardModal } from "../cmps/board/create-board-modal";
+import { ReactComponent as Clock } from '../assets/icon/dates-icon.svg'
+import { ReactComponent as Star } from '../assets/icon/star.svg'
 
 class _HomePage extends React.Component {
     state = {
@@ -31,16 +33,39 @@ class _HomePage extends React.Component {
         this.setState(prevState => ({ ...prevState, isModalShown: !isModalShown }))
     }
 
+    onMarkStar=(ev, board)=>{
+        ev.preventDefault()
+        const newBoard = JSON.parse(JSON.stringify(board))
+        
+    }
+
     render() {
         const { boards } = this.props
         const { isModalShown } = this.state
 
         return (
             <section className="home-page">
-                <h1>Our most popular templates</h1>
+                <div className="board-list-container">
+                    <div className="flex">
+                <Star/>
+                <h1>Starred boards</h1>
+                    </div>
+                <div className="flex">
+                    <Clock/>
+                <h1> Recently viewed</h1>
+                </div>
                 <section className="previews-container">
                     {(!boards && !boards.length) && <div>Loading...</div>}
-                    {boards && boards.map((board, idx) => <Link style={{ backgroundImage: `url(${board.style.imgUrl})` }} key={board._id + idx} to={`/board/${board._id}`}><BoardPreview board={board} /></Link>)}
+                    {boards && boards.map((board, idx) => 
+                    <div className="board-preview" style={{ backgroundImage: `url(${board.style.imgUrl})` }}>
+                    <Link  key={board._id + idx} to={`/board/${board._id}`}>
+                        <BoardPreview board={board} />
+                        <span onClick={(ev)=>this.onMarkStar(ev, board)} className="star">
+                        <Star/>
+                        </span>
+                        </Link> 
+                        </div>
+                        )}
 
                     <div onClick={() => this.onToggleModal()} className="create-board flex">
                         <p>Create new board</p>
@@ -49,6 +74,7 @@ class _HomePage extends React.Component {
                 <PopOver cb={this.onToggleModal} isShown={isModalShown} title={'Create board'}>
                     <CreateBoardModal isShown={isModalShown} />
                 </PopOver>
+                </div>
             </section>
         )
     }
