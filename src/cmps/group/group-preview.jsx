da1a4884a43af5e7d10eed6bd42470862be8d964
shlomo lifschitz <shinlamed@gmail.com>
2022-06-06 09:14:10 +0300

    /bug

--- src/cmps/group/group-preview.jsx
+++ src/cmps/group/group-preview.jsx
@@ -19,7 +19,6 @@ export const GroupPreview = ({ group, boardId, onToggleDetails, setIsLabelOpen, 
     const dispatch = useDispatch()
     const { currBoard } = useSelector(state => state.boardModule)
     const groupIdx = currBoard.groups.findIndex(currGroup => currGroup.id === group.id)
-<<<<<<< HEAD
     const [groupTitle, setGroupTitle] = useState()
     const onChangeName = (val) => {
         let groupToUpdate = utilService.getDeepCopy(group)
@@ -42,14 +41,6 @@ export const GroupPreview = ({ group, boardId, onToggleDetails, setIsLabelOpen, 
         groupToUpdate.title = groupTitle
         dispatch(onUpdateGroup(boardId, groupToUpdate))
 
-=======
-    const onChangeName = (val) => {
-        try {
-            dispatch(onUpdateGroup(boardId, group.id, val))
-        } catch (err) {
-            throw err
-        }
->>>>>>> dacf6d033fa48e881642bcfd72b76d157c2929d9
     }
     const onSendToOpen = (groupId) => {
         eventBusService.emit('open-group-modal', groupId)
@@ -75,7 +66,6 @@ export const GroupPreview = ({ group, boardId, onToggleDetails, setIsLabelOpen, 
                     className="group-preview" key={group.id}>
 
                     <div className="header-container">
-<<<<<<< HEAD
                         <GroupHeader
                             key={group.id}
                             onSubmit={onSubmitTitle}
@@ -84,12 +74,6 @@ export const GroupPreview = ({ group, boardId, onToggleDetails, setIsLabelOpen, 
                             title={groupTitle}
                         />
                         <button onClick={() => onDeleteGroup(group.id)}>
-=======
-
-                        <GroupHeader key={group.id} onChangeName={onChangeName}
-                            title={group.title} />
-                        <button onClick={() => onSendToOpen(group.id)}>
->>>>>>> dacf6d033fa48e881642bcfd72b76d157c2929d9
                             <span>
                                 <ThreeDots style={{ width: '15px' }} />
                             </span>
